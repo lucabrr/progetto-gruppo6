897f47166b792be1251ada18dee22f8cb5d8d8e1
MarLizZo <marketto140@libero.it>
2023-04-17 19:44:28 +0200

    js renamed

--- project/js/quiz.js
+++ project/js/quiz.js
@@ -9,19 +9,19 @@
 //     })
 // })()
 
+let questionsArr = [];
+
 async function getQuestions() {
     fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy').then(async function(res) {
-        buildQuiz(await res.json());
+        let json = await res.json();
+        questionsArr = json.results;
+        buildQuiz();
     })
 }
 
-let questionsArr = [];
-
-function buildQuiz(arr) {
+function buildQuiz() {
     //const target = document.getElementById('target');
     //let buttons = target.querySelectorAll('button');
-    console.log(arr);
-    questionsArr = arr.results;
     console.log(questionsArr);
 }
 
