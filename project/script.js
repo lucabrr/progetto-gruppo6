5fdf2e0ef03a4644e9929d62f5d73cf881fbdd93
Manuel-Altruda <manuelaltruda8@gmail.com>
2023-04-17 17:14:43 +0200

    .

--- project/script.js
+++ project/script.js
@@ -1,4 +1,31 @@
+// (function acceptButton() {
+//     const btn = document.getElementById('acceptBtn');
+//     const checkB = document.getElementById('checkB');
+//     btn.addEventListener('click', () => {
+//         if (checkB.checked) {
+//             //do destroy
+//             //call build function
+//         }
+//     })
+// })()
 
+async function getQuestions() {
+  fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy').then(async function(res) {
+      buildQuiz(await res.json());
+  })
+}
+
+let questionsArr = [];
+
+function buildQuiz(arr) {
+  //const target = document.getElementById('target');
+  //let buttons = target.querySelectorAll('button');
+  console.log(arr);
+  questionsArr = arr.results;
+  console.log(questionsArr);
+}
+
+getQuestions();
 
 // impostazione delle domande
 const questions = [ {
