8c24a5a96330cc76e24379878eef5ae7fc440de4
MarLizZo <marketto140@libero.it>
2023-04-17 14:17:13 +0200

    js base

---
+++ project/js/script.js
@@ -0,0 +1,19 @@
+(function acceptButton() {
+    const btn = document.getElementById('acceptBtn');
+    const checkB = document.getElementById('checkB');
+    btn.addEventListener('click', () => {
+        if (checkB.checked) {
+            //do destroy
+            //call build function
+        }
+    })
+})()
+
+async function getQuestions() {
+    //do fetch
+}
+
+function buildQuiz(arr) {
+    const target = document.getElementById('target');
+    let buttons = target.querySelectorAll('button');
+}
