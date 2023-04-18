303571a2448526e8b7d01bed5c585af5c76689ef
Manuel-Altruda <manuelaltruda8@gmail.com>
2023-04-18 12:32:52 +0200

    newPushTimer

--- project/js/timer.js
+++ project/js/timer.js
@@ -1,20 +1,25 @@
 
 
-let tempoTotale = 60;
+  let tempoTotale = 60;
 
-//seleziona l'elemento HTML dove visualizzare il timer
-let timer = document.getElementById("timer");
+  //seleziona l'elemento HTML dove visualizzare il timer
+  let timer = document.getElementById("timer");
 
-//imposta la funzione per aggiornare il timer
-let updateTimer = setInterval(function() {
-  tempoTotale--;
-  timer.textContent = tempoTotale;
-  if (tempoTotale <= 0) {
-    clearInterval(updateTimer);
-    alert("Tempo scaduto!");
-    
-  }
-}, 1000); //la funzione viene eseguita ogni secondo (1000 millisecondi)
+  //imposta la funzione per aggiornare il timer
+  let updateTimer = setInterval(function() {
+
+    tempoTotale--;
+
+    timer.textContent = tempoTotale;
+
+    if (tempoTotale <= 0) {
+
+      clearInterval(updateTimer);
+      
+      alert("Tempo scaduto!");
+      
+    }
+  }, 1000); //la funzione viene eseguita ogni secondo (1000 millisecondi)
 
 
   
