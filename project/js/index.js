6572356d0aad28c6ff67e57f57253de0c9ce4a93
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-04-19 10:13:42 +0200

    adjustments

--- project/js/index.js
+++ project/js/index.js
@@ -1,13 +1,18 @@
 // import './modules/quiz.js';
 
+const avviso = document.getElementById('avviso');
+
 (function acceptButton() {
     const btn = document.getElementById('acceptBtn');
     const checkB = document.getElementById('consenso');
+
     btn.addEventListener('click', () => {
-        if (checkB.checked) {
+        if (!checkB.checked) {
+            avviso.classList.remove('avviso')
+
+        } else if (checkB.checked) {
             location.href = "../questions.html"
-        } else {
-            alert("Please check che condition checkbox !")
+
         }
     })
 })()
