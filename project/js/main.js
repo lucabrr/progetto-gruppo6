f549fbeb2c0dac05138ba5031be52b56e906df31
MarLizZo <marketto140@libero.it>
2023-04-21 11:37:41 +0200

    easter p1

--- project/js/main.js
+++ project/js/main.js
@@ -30,9 +30,17 @@
 
     let easterEgg = document.querySelector('#easter');
     let audioEgg = document.querySelector('#audioIntro');
+    let videoEgg = document.querySelector('#videoIntro');
     easterEgg.addEventListener('click', function () {
-        audioEgg.play();
-    })
+      audioEgg.play()
+      setTimeout(function () {
+          videoEgg.classList.remove('scomparsa');
+          videoEgg.play();
+      }, 21000)
+      setTimeout(function () {
+          videoEgg.classList.add('scomparsa');
+      }, 35000)
+  })
 })()
 
 
